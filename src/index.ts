9434d3d2aa5ae3022eac38332f5b99bd4838212c
CharlesSigo <chuck@schooldatebooks.com>
2019-10-29 13:54:39 -0400

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {FrameworkConfiguration} from 'aurelia-framework';
 import {PLATFORM} from 'aurelia-pal';
 
+export {DragAndDropController} from './elements/drag-and-drop-controller';
+
 export function configure(config: FrameworkConfiguration) {
   config.globalResources([
     PLATFORM.moduleName('./elements/data-model-custom-attribute')
